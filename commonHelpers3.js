b99d855f4ed3a2e402eef807ccee353191660c62
IvyMtsh <147334689+IvyMtsh@users.noreply.github.com>
2024-03-08 16:41:11 +0000

    Deploying to gh-pages from @ IvyMtsh/goit-js-hw-09@c9dd284d5e520fdefd6d22b798314ac37a405f56 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/styles-33b2e339.js";const n=document.querySelectorAll(".linkList");n.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();const r=e.querySelector("a").getAttribute("href");window.location.href=r})});
+//# sourceMappingURL=commonHelpers3.js.map
